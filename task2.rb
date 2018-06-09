1f5928ce1fa532047157a25fa71c993ce4bbad88
Ruslan Zagidullin <zagidullinruslan.gs@gmail.com>
2018-06-10 00:35:23 +0500

    task2

--- task2.rb
+++ task2.rb
@@ -1,14 +1,45 @@
-if ARGV.length != 2
-  puts false
-  exit
+if ARGV.length != 3
+	exit
 end
 
-arg1 = ARGV[0].to_i 
-arg2 = ARGV[1].to_i 
+day = ARGV[0].to_i 
+monthString = ARGV[1]
+year = ARGV[2].to_i
 
-if arg1 >= 10000 || arg1 <= 1 || arg2 >= 10000 || arg2 <= 1
-  puts false
-  exit
+monthsArray = ['января', 'февраля', 'марта', 'апреля', 'мая', 'июня', 'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря']
+
+month = monthsArray.index(monthString)
+
+if day  < 1 || day > 31
+	exit
 end
- 
-puts arg1 % arg2 > 0 && arg2 % arg1 > 0 ? true : false 
+
+if !month
+	exit
+end
+
+if year < 1 || year > 2999
+	exit
+end
+
+month += 1
+
+def g(y)
+	return (1 - ((y%4 + 2)/(y%4 + 1))) * ((y%100 + 2)/(y%100 + 1)) + (1 - ((y%400 + 2)/(y%400 + 1)))
+end
+
+def f(m,y)
+	return 28 + (m + (m/8))%2 + 2%m + ((1 + g(y)) / m) + 1/m - g(y)/m
+end	
+
+totalCountOfDaysInSelectedMonth = f(month, year) 
+
+countBuf = totalCountOfDaysInSelectedMonth - day 
+
+month += 1
+
+for i in month..12
+countBuf += f(i, year)
+end
+
+puts countBuf
