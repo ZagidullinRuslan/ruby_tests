16b9e86e6e4e522dd6f871e0f92a6e25ee8d583f
Ruslan Zagidullin <zagidullinruslan.gs@gmail.com>
2018-06-10 00:47:08 +0500

    task3 firs part

--- task3.rb
+++ task3.rb
@@ -1,14 +1,12 @@
 if ARGV.length != 2
-  puts false
   exit
 end
 
 arg1 = ARGV[0].to_i 
 arg2 = ARGV[1].to_i 
 
-if arg1 >= 10000 || arg1 <= 1 || arg2 >= 10000 || arg2 <= 1
-  puts false
+if arg1 > 100000 || arg1 < 1 || arg2 > 100000 || arg2 < 1
   exit
 end
  
-puts arg1 % arg2 > 0 && arg2 % arg1 > 0 ? true : false 
+totalSeconds = arg1 + arg2
