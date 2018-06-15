fe39482068f8fdc3bd45d77176a6e5e61a99af0e
Zagidullin Ruslan <zagidullinruslan.gs@gmail.com>
2018-06-15 13:23:48 +0500

    punctuation delete

--- task1.rb
+++ task1.rb
@@ -3,7 +3,8 @@ if string.length == 1 || string.length == 0
 	puts "YES"
 	exit
 end
-string = string.gsub(/\s+/, '')
+string = string.gsub(/\s+/, '') 
+string = string.gsub(/[^\p{L}\s\d]/,'')
 string.downcase!
 reverseString = ""
 index = string.length
