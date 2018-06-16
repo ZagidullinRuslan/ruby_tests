13893caf439c71a23ffce301c3ab8d8fd6055d8f
Zagidullin Ruslan <zagidullinruslan.gs@gmail.com>
2018-06-16 21:20:00 +0500

    task 2 fix

--- task2.rb
+++ task2.rb
@@ -2,18 +2,21 @@ day = ARGV[0].to_i
 monthString = ARGV[1]
 year = ARGV[2].to_i
 monthsArray = ['января', 'февраля', 'марта', 'апреля', 'мая', 'июня', 'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря']
+$daysInMounthsAray = [31,28,31,30,31,30,31,31,30,31,30,31]
 month = monthsArray.index(monthString)
-month += 1
-def g(y)
-	return (1 - ((y%4 + 2)/(y%4 + 1))) * ((y%100 + 2)/(y%100 + 1)) + (1 - ((y%400 + 2)/(y%400 + 1)))
-end
-def f(m,y)
-	return 28 + (m + (m/8))%2 + 2%m + ((1 + g(y)) / m) + 1/m - g(y)/m
-end	
-totalCountOfDaysInSelectedMonth = f(month, year) 
+def getDaysCount(m,y)
+	if m != 1 # февраль
+		return $daysInMounthsAray[m]
+	end	
+	if y%400==0 || (y%100!=0 && y%4==0) 
+		return 	$daysInMounthsAray[m] + 1
+	end 
+	return $daysInMounthsAray[m]
+end 
+totalCountOfDaysInSelectedMonth = getDaysCount(month, year) 
 countBuf = totalCountOfDaysInSelectedMonth - day 
 month += 1
-for i in month..12
-countBuf += f(i, year)
+for i in month..11
+countBuf += getDaysCount(i, year)
 end
 puts countBuf
